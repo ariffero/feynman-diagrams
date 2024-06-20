63523787ef88ef7534238a58d7dcb026e6905fb5
ariffero <andrea.giovanii.riffero@cern.ch>
2024-06-20 11:30:39 +0200

    Add macro for inclusive process

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ ROOT macros to draw some Feyman diagrams
 2. **photonFusionX.c** macro to produce a diagram of photon-photon fusion into a generic state x. Interaction representat as a blob. Portrait orientation. It has an option to draw the final state with an arrow (default) or with a line, for the second execute the script using passing *false* as parameter.
 
 3. **diffractive.c** macro that produces a diagram for a diffractive photon-nulcear interaction that produces a general final state x. Interaction represented as a blob. Portrait orientation. It has an option to draw the final state with an arrow (default) or with a line, for the second execute the script using passing *false* as parameter.
+
+4. **inclusive.c** macro that produces a diagram for an inclusive photon-nulcear interaction that produces a general final state x. Interaction represented as a blob. Portrait orientation. It has an option to draw the final state products with arrows (default) or with lines, for the second execute the script using passing *false* as parameter.
---
+++ inclusive.c
@@ -0,0 +1,65 @@
+// draw the Feynman diagram for photon-nuclear inclusive process in UPCs
+
+void inclusive(bool directions = true){
+
+    TCanvas *c = new TCanvas("c", "c", 10,10, 400, 600);
+    c->Range(0, 0, 80, 100);
+    Int_t linsav = gStyle->GetLineWidth();
+    gStyle->SetLineWidth(2);
+    TLatex t;
+    t.SetTextAlign(22);
+    t.SetTextSize(0.07);
+    t.SetTextFont(22);
+
+    //line for lead
+    TLine *lead;
+
+    //wave for photon
+    TCurlyLine *gamma = new TCurlyLine(); 
+    gamma->SetWavy();
+    gamma->Draw();
+
+    //line for final state x
+    TArrow *final;
+    float arrowSize = 0.02;
+    if(!directions) arrowSize = 0;
+
+    //upper lead
+    lead = new TLine(10, 80, 30, 70); 
+    lead->Draw();
+    lead = new TLine(30, 70, 70, 80); 
+    lead->Draw();
+    t.DrawLatex(15,83,"A");
+    t.DrawLatex(65,83,"A");
+
+    //lower lead
+    lead = new TLine(10, 20, 40, 30); 
+    lead->Draw();
+    lead = new TLine(40, 30, 70, 20); 
+    lead->Draw();
+    t.DrawLatex(15,17,"A");
+    t.DrawLatex(65,17,"B");
+
+    // photon
+    gamma = new TCurlyLine(30, 70, 40, 30);
+    gamma->Draw();
+    t.DrawLatex(27,62,"#gamma");
+
+    //final state x
+    final = new TArrow(39.5, 33., 57, 40., arrowSize, "-->");
+    final->Draw();
+    final = new TArrow(40, 31., 61, 27., arrowSize, "-->");
+    final->Draw();
+    final = new TArrow(40, 31.5, 60, 34., arrowSize, "-->");
+    final->Draw();
+    
+    t.DrawLatex(66,35.,"x");
+    
+    //interaction
+    TEllipse *interaction = new TEllipse(40, 32, 3, 5);
+    interaction->SetNoEdges();
+    interaction->SetFillColor(kGray);
+    interaction->SetLineWidth(0);
+    interaction->Draw();
+        
+}
