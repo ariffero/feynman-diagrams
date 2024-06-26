7683e1a7e855723be4f353b33a5b50f2dfd2b3cc
ariffero <andrea.giovanii.riffero@cern.ch>
2024-06-26 14:43:50 +0200

    Add rapidity gaps

--- diffractiveExclusive.c
+++ diffractiveExclusive.c
@@ -1,6 +1,6 @@
 // draw the Feynman diagram for photon-nuclear inclusive process in UPCs
 
-void inclusive(bool directions = true){
+void diffractiveExclusive(bool directions = true){
 
     TCanvas *c = new TCanvas("c", "c", 10,10, 400, 600);
     c->Range(0, 0, 80, 100);
@@ -38,39 +38,65 @@ void inclusive(bool directions = true){
     lead = new TLine(40, 30, 70, 20); 
     lead->Draw();
     t.DrawLatex(15,17,"A");
-    t.DrawLatex(65,17,"B");
+    t.DrawLatex(65,17,"A");
 
     // photon
-    gamma = new TCurlyLine(30, 70, 40, 30);
+    gamma = new TCurlyLine(30, 70, 37, 47);
     gamma->SetWavy();
     gamma->Draw();
     t.DrawLatex(27,62,"#gamma");
 
+
+    //pomeron
+    TLine *pomeron = new TLine(39.5, 29.8, 37, 40);
+    pomeron->SetLineStyle(2);
+    pomeron->Draw();
+    pomeron = new TLine(40.5, 29.8, 38, 40);
+    pomeron->SetLineStyle(2);
+    pomeron->Draw();
+
     //final state x
-    final = new TArrow(39.5, 33., 57, 40., arrowSize, "-->");
+    //final = new TArrow(37, 42., 60, 38., arrowSize, "-->");
+    //final->Draw();
+    //final = new TArrow(37, 43.5, 61, 43., arrowSize, "-->");
+    //final->Draw();
+    //final = new TArrow(37, 45., 57, 47., arrowSize, "-->");
+    //final->Draw();
+    final = new TArrow(37, 44, 55, 44., arrowSize, "-->");
     final->Draw();
-    final = new TArrow(40, 31., 61, 27., arrowSize, "-->");
-    final->Draw();
-    final = new TArrow(40, 31.5, 60, 34., arrowSize, "-->");
-    final->Draw();
-    
-    t.DrawLatex(66,35.,"x");
-    
+
+    t.DrawLatex(60,44.,"x");
+
     //interaction
-    TEllipse *interaction = new TEllipse(40, 32, 3, 5);
+    TEllipse *interaction = new TEllipse(37.5, 44, 3, 5);
     interaction->SetNoEdges();
     interaction->SetFillColor(kGray);
     interaction->SetLineWidth(0);
     interaction->Draw();
 
+    t.DrawLatex(34,34,"#Rho");
+
+    //TLatex gaps;
+    //gaps.SetTextAlign(22);
+    //gaps.SetTextSize(0.18);
+    //gaps.SetTextFont(152);
+    //gaps.DrawLatex(50,34,"}");
+    //gaps.SetTextSize(0.3);
+    //gaps.DrawLatex(50,60,"}");
+    //TLatex rap;
+    //t.DrawLatex(58,34,"gap");
+
     //mark the rapidity gap
-    TArrow *rap = new TArrow(50,60,60,60,arrowSize,"<-|");
+    TArrow *rap = new TArrow(50,34,60,34,arrowSize,"<-|");
+    rap->Draw();
+    t.DrawLatex(66, 34, "gap");
+    rap = new TArrow(50, 60, 60, 60,arrowSize,"<-|");
     rap->Draw();
     t.DrawLatex(66, 60, "gap");
 
     //save the image
     gSystem->mkdir("images");
-    c->SaveAs("images/inclusive.pdf");
+    c->SaveAs("images/diffractiveExclusive.pdf");
 
         
 }
